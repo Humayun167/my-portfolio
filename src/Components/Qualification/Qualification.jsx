8f8680190e5091fa2d14b56f4164aa02be837dca
Md Humayun Rashid Khan <humayunrashidkhan96@gmail.com>
2025-07-11 16:53:22 +0600

    updated

--- src/Components/Qualification/Qualification.jsx
+++ src/Components/Qualification/Qualification.jsx
@@ -5,6 +5,11 @@ const Qualification = () => {
         <div className="qualification">
             <h2>Qualification</h2>
             <div className="qualification-item">
+                 <div className="qualification-item">
+                <h3>B.Sc in Computer Science & Engineering</h3>
+                <p>2022-Present</p>
+                <p>Daffodil International University</p>
+            </div>
                 <h3>Secondary School Certificate</h3>
                 <p>2017</p>
                 <p>Darussunnat Kamil Madrasah, Habiganj</p>
@@ -14,11 +19,7 @@ const Qualification = () => {
                 <p>2019</p>
                 <p>BAF Shaheen College, Shamshernagar</p>
             </div>
-            <div className="qualification-item">
-                <h3>B.Sc in Computer Science & Technology</h3>
-                <p>2022-Present</p>
-                <p>Daffodil International University</p>
-            </div>
+           
         </div>
     );
 };
